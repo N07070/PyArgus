e081458eab04c733b4846109b6a0449ba2db075e
N07070 <nelson.perdriau@gmail.com>
2015-03-29 19:53:47 +0200

    Made a little progress : changed the langage, and added a way to stop the system with the voice.
    I'm sorry Dave, I'm afraid I can't do that.

--- argus.py
+++ argus.py
@@ -29,6 +29,23 @@ close_door = "close the door"
 turn_on_lights = "turn on the lights"
 turn_off_lights = "turn the lights off"
 play_music = "play music"
+stop_system = "stop the system"
+
+def init_GPIO():
+    # Init the GPIO of the RPi
+    prin("hey")
+
+
+def GPIO_state(state):
+    if state == "high":
+        # Send power -> Need to check that theire is no power before ?
+        print("Setting the power to HIGH")
+    if state == "low":
+        # Cut power -> Need to check that theire is actual power ?
+        print("Setting the power to LOW")
+    if state == "pulse":
+        # Send a pulse of electricity
+        print("Sent a pulse of power.")
 
 def door(state):
     # Import GPIO to open the door.
@@ -39,10 +56,19 @@ def door(state):
     else:
         print("I can't do shit.")
 
+def lights(state):
+    if state == "on":
+        print("Let they be light !")
+    elif state == "off":
+        print("I am turning them lights off")
+    else:
+        print("I can't do shit.")
+
+
 def main():
     global fail_counter
     # Set the language of the recognition
-    r = sr.Recognizer(language = "fr-FR")
+    r = sr.Recognizer(language = "en-US")
     m = sr.Microphone()
 
     while True:
@@ -50,10 +76,11 @@ def main():
             print("Listing..")
             audio = r.listen(source)
             try:
-                recognised=r.recognize(audio)
+                recognised = r.recognize(audio)
                 print("You said : %s" % (recognised))
             except LookupError:
                 print("I did not understand, could you say that again ?")
+                recognised = "Error"
 
         if open_door in recognised.lower():
             print('I got that ! You asked for : %s' % recognised)
@@ -61,6 +88,9 @@ def main():
         elif close_door in recognised.lower():
             print('I got that ! You asked for : %s' % recognised)
             door("close")
+        elif stop_system in recognised.lower():
+            print("Shutting down !")
+            sys.exit()
         else:
             pass
 
